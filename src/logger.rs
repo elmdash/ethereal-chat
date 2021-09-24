2b9971119ea9ffa47317f7e77363ce90611af279
Peter Sooley <peter.sooley@gmail.com>
2021-09-23 21:58:17 -0400

    basic server

--- src/logger.rs
+++ src/logger.rs
@@ -1,5 +1,3 @@
-use tracing_subscriber::fmt::format::FmtSpan;
-
 pub fn init() {
     tracing_subscriber::fmt().without_time().init();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 use ethereal_chat::logger;
-use tracing::{info, debug, instrument};
-
-#[instrument]
-async fn hello() {
-    println!("hello");
-    info!("hello handled");
-}
+use tracing::info;
+use axum::Router;
+use axum::handler::get;
+use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
     logger::init();
 
-    info!(test = 1, "trace info");
-    let x = hello();
-    debug!("trace debug");
-    x.await;
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 5000));
+    info!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
